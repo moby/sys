5e0b977a6738e75a3410dc209bf85786dad89abc
Tonis Tiigi <tonistiigi@gmail.com>
2017-07-02 21:21:40 -0700

    buildd: graceful shutdown on termination signals
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

---
+++ appcontext/appcontext.go
@@ -0,0 +1,42 @@
+package appcontext
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+
+	"github.com/Sirupsen/logrus"
+	"golang.org/x/sys/unix"
+)
+
+var appContextCache context.Context
+var appContextOnce sync.Once
+
+// Context returns a static context that reacts to termination signals of the
+// running process. Useful in CLI tools.
+func Context() context.Context {
+	appContextOnce.Do(func() {
+		signals := make(chan os.Signal, 2048)
+		signal.Notify(signals, unix.SIGTERM, unix.SIGINT)
+
+		const exitLimit = 3
+		retries := 0
+
+		ctx, cancel := context.WithCancel(context.Background())
+		appContextCache = ctx
+
+		go func() {
+			for {
+				<-signals
+				cancel()
+				retries++
+				if retries >= exitLimit {
+					logrus.Errorf("got %d SIGTERM/SIGINTs, forcing shutdown", retries)
+					os.Exit(1)
+				}
+			}
+		}()
+	})
+	return appContextCache
+}
