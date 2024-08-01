moby/sys

commit a8e5adca6a64ec29a39473d406c43f7bbfa0699d

capability: Fix future version compatibility
In case kernel folks will ever release capability v4, the chances are
high v3 is still supported. Therefore, we should not error out upon
seeing an unknown version from the kernel, but assume we can go with v3.

While at it, treat the uninitialized capVers as an error. Before this
patch, it was still treated as an error, but "unknown capability version"
is not exactly what the error is, so let's be more specific.

Reported-by: Andrei Vagin <avagin@google.com>
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

capability/capability_linux.go
@@ -16,8 +16,6 @@ import (
 	"syscall"
 )
 
-var errUnknownVers = errors.New("unknown capability version")
-
 const (
 	linuxCapVer1 = 0x19980330
 	linuxCapVer2 = 0x20071026
@@ -100,19 +98,21 @@ func mkString(c Capabilities, max CapType) (ret string) {
 
 func newPid(pid int) (c Capabilities, err error) {
 	switch capVers {
+	case 0:
+		err = errors.New("unable to get capability version from the kernel")
 	case linuxCapVer1:
 		p := new(capsV1)
 		p.hdr.version = capVers
 		p.hdr.pid = int32(pid)
 		c = p
-	case linuxCapVer2, linuxCapVer3:
+	default:
+		// linuxCapVer2, linuxCapVer3, or an unknown/future version such as v4.
+		// In the latter case, we fall back to v3 hoping the kernel is
+		// backward-compatible to v3.
 		p := new(capsV3)
 		p.hdr.version = capVers
 		p.hdr.pid = int32(pid)
 		c = p
-	default:
-		err = errUnknownVers
-		return
 	}
 	return
 }

commit 9aa254c557e6f404d08a49b6ac932f37a8dd83f8

capability: Really fix future kernel compatibility
Earlier commit made sure we don't error out if the kernel capability
version is unknown; this ensures compatibility with future kernels.

Looking at the code, I realized p.hdr.version should be initialized to
linuxCapVer3 in that case, not the version returned by the kernel,
otherwise we supply v3 data structure with (say) v4 version set in
header.

Practically, this was not a real bug (yet) because v4 is not (yet)
available, but if it will ever be introduced later, this fix makes us
ready.

Fixes: a8e5adca6a64ec ("Fix future version compatibility")
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

capability/capability_linux.go
@@ -110,11 +110,11 @@ func newPid(pid int) (c Capabilities, retErr error) {
 	case linuxCapVer1, linuxCapVer2:
 		retErr = errors.New("old/unsupported capability version (kernel older than 2.6.26?)")
 	default:
-		// Either linuxCapVer3, or an unknown/future version such as v4.
-		// In the latter case, we fall back to v3 hoping the kernel is
-		// backward-compatible to v3.
+		// Either linuxCapVer3, or an unknown/future version (such as v4).
+		// In the latter case, we fall back to v3 as the latest version known
+		// to this package, as kernel should be backward-compatible to v3.
 		p := new(capsV3)
-		p.hdr.version = ver
+		p.hdr.version = linuxCapVer3
 		p.hdr.pid = int32(pid)
 		c = p
 	}