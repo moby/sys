4fa19e8f422938c2cdd2a1fd406a806ed3416db7
Aleksa Sarai <cyphar@cyphar.com>
2026-03-04 21:53:49 +1100

    devices: drop deprecated aliases from runc
    These are old APIs from runc that we should not export at all.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- devices/device_unix.go
+++ devices/device_unix.go
@@ -27,6 +27,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/opencontainers/cgroups/devices/config"
 	"golang.org/x/sys/unix"
 )
 
@@ -42,7 +43,7 @@ var (
 // DeviceFromPath takes the path to a device and its cgroup_permissions (which
 // cannot be easily queried) to look up the information about a linux device
 // and returns that information as a Device struct.
-func DeviceFromPath(path, permissions string) (*Device, error) {
+func DeviceFromPath(path, permissions string) (*config.Device, error) {
 	var stat unix.Stat_t
 	err := unixLstat(path, &stat)
 	if err != nil {
@@ -50,7 +51,7 @@ func DeviceFromPath(path, permissions string) (*Device, error) {
 	}
 
 	var (
-		devType   Type
+		devType   config.Type
 		mode      = stat.Mode
 		devNumber = uint64(stat.Rdev) //nolint:unconvert // Rdev is uint32 on e.g. MIPS.
 		major     = unix.Major(devNumber)
@@ -58,20 +59,20 @@ func DeviceFromPath(path, permissions string) (*Device, error) {
 	)
 	switch mode & unix.S_IFMT {
 	case unix.S_IFBLK:
-		devType = BlockDevice
+		devType = config.BlockDevice
 	case unix.S_IFCHR:
-		devType = CharDevice
+		devType = config.CharDevice
 	case unix.S_IFIFO:
-		devType = FifoDevice
+		devType = config.FifoDevice
 	default:
 		return nil, ErrNotADevice
 	}
-	return &Device{
-		Rule: Rule{
+	return &config.Device{
+		Rule: config.Rule{
 			Type:        devType,
 			Major:       int64(major),
 			Minor:       int64(minor),
-			Permissions: Permissions(permissions),
+			Permissions: config.Permissions(permissions),
 		},
 		Path:     path,
 		FileMode: os.FileMode(mode &^ unix.S_IFMT),
@@ -81,18 +82,18 @@ func DeviceFromPath(path, permissions string) (*Device, error) {
 }
 
 // HostDevices returns all devices that can be found under /dev directory.
-func HostDevices() ([]*Device, error) {
+func HostDevices() ([]*config.Device, error) {
 	return GetDevices("/dev")
 }
 
 // GetDevices recursively traverses a directory specified by path
 // and returns all devices found there.
-func GetDevices(path string) ([]*Device, error) {
+func GetDevices(path string) ([]*config.Device, error) {
 	files, err := osReadDir(path)
 	if err != nil {
 		return nil, err
 	}
-	var out []*Device
+	var out []*config.Device
 	for _, f := range files {
 		switch {
 		case f.IsDir():
@@ -123,7 +124,7 @@ func GetDevices(path string) ([]*Device, error) {
 			}
 			return nil, err
 		}
-		if device.Type == FifoDevice {
+		if device.Type == config.FifoDevice {
 			continue
 		}
 		out = append(out, device)
--- devices/device_unix_test.go
+++ devices/device_unix_test.go
@@ -28,6 +28,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/opencontainers/cgroups/devices/config"
 	"golang.org/x/sys/unix"
 )
 
@@ -105,8 +106,8 @@ func TestHostDevicesAllValid(t *testing.T) {
 			t.Errorf("device entry %+v has zero major number", device)
 		}
 		switch device.Type {
-		case BlockDevice, CharDevice:
-		case FifoDevice:
+		case config.BlockDevice, config.CharDevice:
+		case config.FifoDevice:
 			t.Logf("fifo devices shouldn't show up from HostDevices")
 			fallthrough
 		default:
